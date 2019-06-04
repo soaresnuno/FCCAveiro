6571e51fad5d7c545f4d74435a9f30a3a70c01ac
Nuno Pereira <nunocpereira@outlook.com>
2019-06-04 14:14:32 +0100

    add utilities

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,5 +1,7 @@
-import colors from './colors';
+import colors from './Colors';
+import elevation from "./Elevation";
 
-export * from './colors';
+export * from './Colors';
+export * from "./Breakpoints";
 
-export { colors };
+export { colors, elevation };
